e6205e94d0cfa6b561f430c6ea410846d0ea63b8
Jakub Radlak <jakubradlak@simple.com.pl>
2021-11-27 20:37:54 +0100

    text rendering refactor

---
+++ TextRenderer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <gl/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <map>
+#include <string>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "Shader.h"
+
+struct Character {
+    unsigned int textureID; 
+    glm::ivec2   size;      
+    glm::ivec2   bearing;   
+    unsigned int advance;
+};
+
+class TextRenderer
+{
+public:
+    TextRenderer(void);
+
+    int init(unsigned int scrWidth, unsigned int scrHeight);
+
+    void renderText(std::string text, float x, float y, float scale, glm::vec3 color);
+
+    ~TextRenderer();
+
+private:
+    Shader* shader;
+
+    std::map<GLchar, Character> Characters;
+    unsigned int VAO, VBO;
+
+    void clear();
+};
--- main.cpp
+++ main.cpp
@@ -14,42 +14,25 @@
 #include <thread>
 #include <chrono> 
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
 #include "Camera.h"
 #include "Window.h"
 #include "CelestialBody.h"
 #include "Rocket.h"
-#include "Shader.h"
-
-void RenderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color);
+#include "TextRenderer.h"
 
 // settings
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
 
-/// Holds all state information relevant to a character as loaded using FreeType
-struct Character {
-    unsigned int TextureID; // ID handle of the glyph texture
-    glm::ivec2   Size;      // Size of glyph
-    glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
-    unsigned int Advance;   // Horizontal offset to advance to next glyph
-};
-
-std::map<GLchar, Character> Characters;
-unsigned int VAO, VBO;
-
 Camera camera(glm::vec3(-100.0, -160.0, 1000.0));
 
 unsigned __int64 currentTime();
+void switchGLStateForWorldRendering();
+void renderTextHUD(TextRenderer* text, Rocket& rocket);
 void syncFramerate(unsigned __int64 startTime, int ms_per_update);
-
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
     Window mainWindow(camera, SCR_WIDTH, SCR_HEIGHT);
     int result = mainWindow.initialize();
     if (result != 0)
@@ -57,106 +40,13 @@ int main()
         return result;
     }
 
-    // OpenGL state
-    // ------------
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    // compile and setup the shader
-    // ----------------------------
-    Shader shader("text_vs.glsl", "text_fs.glsl");
-    glm::dmat4 projectionTelemetry = glm::ortho(0.0, static_cast<double>(SCR_WIDTH), 0.0, static_cast<double>(SCR_HEIGHT));
-    shader.use();
-    shader.setMat4("projectionTelemetry", projectionTelemetry);
-
-    // FreeType
-    // --------
-    FT_Library ft;
-    // All functions return a value different than 0 whenever an error occurred
-    if (FT_Init_FreeType(&ft))
+    TextRenderer* text = new TextRenderer;
+    result = text->init(SCR_WIDTH, SCR_HEIGHT);
+    if (result != 0)
     {
-        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-        return -1;
+        return result;
     }
 
-
-    // load font as face
-    FT_Face face;
-    if (FT_New_Face(ft, "fonts/Antonio-Bold.ttf", 0, &face)) {
-        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-        return -1;
-    }
-    else {
-        // set size to load glyphs as
-        FT_Set_Pixel_Sizes(face, 0, 48);
-
-        // disable byte-alignment restriction
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-        // load first 128 characters of ASCII set
-        for (unsigned char c = 0; c < 128; c++)
-        {
-            // Load character glyph 
-            if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-            {
-                std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-                continue;
-            }
-            // generate texture
-            unsigned int texture;
-            glGenTextures(1, &texture);
-            glBindTexture(GL_TEXTURE_2D, texture);
-            glTexImage2D(
-                GL_TEXTURE_2D,
-                0,
-                GL_RED,
-                face->glyph->bitmap.width,
-                face->glyph->bitmap.rows,
-                0,
-                GL_RED,
-                GL_UNSIGNED_BYTE,
-                face->glyph->bitmap.buffer
-            );
-            // set texture options
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            // now store character for later use
-            Character character = {
-                texture,
-                glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-                glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-                static_cast<unsigned int>(face->glyph->advance.x)
-            };
-            Characters.insert(std::pair<char, Character>(c, character));
-        }
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
-    // destroy FreeType once we're finished
-    FT_Done_Face(face);
-    FT_Done_FreeType(ft);
-
-
-    // configure VAO/VBO for texture quads
-    // -----------------------------------
-  
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-
-    // ================================================== END OF FONT INITIALIZATION ==========================
-
-    // ========================================= BEGIN OF 3D WORLD INITIALIZATION =============================
-    
     glm::dmat4 projection = glm::perspective((double)glm::radians(camera.Zoom),
         (double)SCR_WIDTH / (double)SCR_HEIGHT, 0.001, 150000000.0);
 
@@ -181,8 +71,7 @@ int main()
     rocket.init();
     rocket.updateRotation(glm::dvec3(-30.0, 0.0, 0.0));
 
-
-    // render loop
+    // simulation loop
     // -----------
     unsigned __int64 lag = 0, previous = currentTime();
     int MS_PER_UPDATE = 12;
@@ -199,22 +88,14 @@ int main()
 
         while (lag > MS_PER_UPDATE)
         {
-            //rocket.updatePhysics(MS_PER_UPDATE / 1000.0f);
+            rocket.updatePhysics(MS_PER_UPDATE / 1000.0f);
             lag -= MS_PER_UPDATE;
         }
 
-        // render
-        // ------
-        glEnable(GL_DEPTH_TEST);
-        glDisable(GL_CULL_FACE);
-        glDisable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        switchGLStateForWorldRendering();
 
         // camera/view transformation
-        //camera.Position = rocket.getPosition() + glm::dvec3(0.0, 0.024, 0.0);
+        camera.Position = rocket.getPosition() + glm::dvec3(0.0, 0.024, 0.0);
         glm::dmat4 view = camera.GetViewMatrix();
 
         earth.render(projection, view, lightPos);
@@ -223,22 +104,8 @@ int main()
 
         rocket.updateRotation(glm::dvec3(10.0, 10.0, 10.0));
         rocket.render(projection, view, lightPos);
-        
-        // render HUD (text)
-        glEnable(GL_CULL_FACE);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glDisable(GL_DEPTH_TEST);
-        
-        glm::dvec3 position = rocket.getPosition();              
-        std::stringstream ssPosition;
-        ssPosition << "This is rocket position: (" << position.x << ", " << position.y << "," << position.y << ")";
-        RenderText(shader, ssPosition.str(), 25.0f, 25.0f, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
-        
-        glm::dvec3 velocity = rocket.getVelocity();
-        std::stringstream ssVeloticy;
-        ssVeloticy << "This is rocket velocity: (" << velocity.x << ", " << velocity.y << ",   " << velocity.y << ")";
-        RenderText(shader, ssVeloticy.str(), 25.0f, 50.0f, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
+
+        renderTextHUD(text, rocket);
 
         syncFramerate(current, MS_PER_UPDATE);
         mainWindow.swapBuffers();
@@ -246,54 +113,38 @@ int main()
     }
 
     glfwTerminate();
+    delete text;
+
     return 0;
 }
 
-// render line of text
-// -------------------
-void RenderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color)
+void switchGLStateForWorldRendering()
 {
-    // activate corresponding render state	
-    shader.use();
-    glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
-    glActiveTexture(GL_TEXTURE0);
-    glBindVertexArray(VAO);
+    glEnable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // iterate through all characters
-    std::string::const_iterator c;
-    for (c = text.begin(); c != text.end(); c++)
-    {
-        Character ch = Characters[*c];
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
 
-        float xpos = x + ch.Bearing.x * scale;
-        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+void renderTextHUD(TextRenderer* text, Rocket& rocket)
+{
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_DEPTH_TEST);
 
-        float w = ch.Size.x * scale;
-        float h = ch.Size.y * scale;
-        // update VBO for each character
-        float vertices[6][4] = {
-            { xpos,     ypos + h,   0.0f, 0.0f },
-            { xpos,     ypos,       0.0f, 1.0f },
-            { xpos + w, ypos,       1.0f, 1.0f },
+    glm::dvec3 position = rocket.getPosition();
+    std::stringstream ssPosition;
+    ssPosition << "This is rocket position: (" << position.x << ", " << position.y << "," << position.y << ")";
+    text->renderText(ssPosition.str(), 25.0f, 25.0f, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
 
-            { xpos,     ypos + h,   0.0f, 0.0f },
-            { xpos + w, ypos,       1.0f, 1.0f },
-            { xpos + w, ypos + h,   1.0f, 0.0f }
-        };
-        // render glyph texture over quad
-        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-        // update content of VBO memory
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        // render quad
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
-    }
-    glBindVertexArray(0);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glm::dvec3 velocity = rocket.getVelocity();
+    std::stringstream ssVeloticy;
+    ssVeloticy << "This is rocket velocity: (" << velocity.x << ", " << velocity.y << ",   " << velocity.y << ")";
+    text->renderText(ssVeloticy.str(), 25.0f, 50.0f, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
 }
 
 void syncFramerate(unsigned __int64 startTime, int ms_per_update)
