ec5b9ed067ecb75aa8eb65caba74301df8e86dfe
jradlak <jradlak@gmail.com>
2022-11-14 23:40:55 +0100

    drawing rocket trajectory

--- renderer/implementation/TrajectoryPrediction.cpp
+++ renderer/implementation/TrajectoryPrediction.cpp
@@ -19,9 +19,15 @@ void TrajectoryPrediction::initWithPositions(
 
 	for (int i = 0; i < px.size(); i++)
 	{
-		Sphere* sphere = new Sphere(1000, 32, 16);
+		Sphere* sphere = new Sphere(0.5, 32, 16);
 		sphere->updatePosition(glm::dvec3(px[i], py[i], pz[i]));
 		spheres.push_back(sphere);
+	}
+
+	if (spheres.size() > 0)
+	{
+		Sphere* sphere = spheres[0];
+		renderer->init(sphere->getVertices(), sphere->getIndices());
 	}
 }
 
@@ -29,7 +35,7 @@ void TrajectoryPrediction::render(glm::dmat4 projection, glm::dmat4 view, const 
 {
 	Shader* shader = renderer->getShader();
 	shader->use();
-	shader->setVec3("objectColor", 0.95f, 0.15f, 0.15f);
+	shader->setVec3("objectColor", 0.85f, 0.85f, 0.85f);
 	shader->setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 	shader->setVec3("lightPos", lightPos);
 
