70d005df887372fdae799ed76d90290a2753d898
jradlak <jradlak@gmail.com>
2024-01-06 23:56:48 +0100

    remove build directory

--- build/CMakeFiles/copy_shaders.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_shaders"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
