ff882e47e04fa0c48aa5279ede7554bc6268f47f
jradlak <jradlak@gmail.com>
2024-01-20 21:07:58 +0100

    use of the memory mutex

--- src/vmachine/Memory.cpp
+++ src/vmachine/Memory.cpp
@@ -2,58 +2,58 @@
 
 unsigned char Memory::fetchByte(unsigned int addr)
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	assertConditions(addr);
-	char val = mem[addr];
-	leaseSemaphore();
+	char val = mem[addr];		
 	return val;
 }
 
 void Memory::storeByte(unsigned int addr, unsigned char byte)
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	assertConditions(addr);
-	mem[addr] = byte;
-	leaseSemaphore();
+	mem[addr] = byte;	
 }
 
 int Memory::fetchWord(unsigned int addr)
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	assertConditions(addr + 4);
 	unsigned char result[4] = { };
 	memcopy(mem, result, addr, 0, 4);
-	unsigned int val = *reinterpret_cast<unsigned int*>(result);
-	leaseSemaphore();
+	unsigned int val = *reinterpret_cast<unsigned int*>(result);	
 	return val;
 }
 
 void Memory::storeWord(unsigned int addr, int word)
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	assertConditions(addr + 4);
 	unsigned char* result = static_cast<unsigned char*>(static_cast<void*>(&word));
-	memcopy(result, mem, 0, addr, 4);
-	leaseSemaphore();
+	memcopy(result, mem, 0, addr, 4);	
 }
 	
 double Memory::fetchDWord(unsigned int addr)
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	assertConditions(addr + 8);
 	unsigned char result[8] = { };
 	memcopy(mem, result, addr, 0, 8);
-	double val = *reinterpret_cast<double*>(result);
-	leaseSemaphore();
+	double val = *reinterpret_cast<double*>(result);	
 	return val;
 }
 
 void Memory::storeDWord(unsigned int addr, double dword)
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	assertConditions(addr + 8);
 	unsigned char* result 
 		= static_cast<unsigned char*>(static_cast<void*>(&dword));
-	memcopy(result, mem, 0, addr, 8);
-	leaseSemaphore();
+	memcopy(result, mem, 0, addr, 8);	
 }
 
 void Memory::memcopy(unsigned char* src, unsigned char* dst, unsigned int addr_s, unsigned int addr_d, unsigned int length)
-{
+{	
 	for (unsigned int i = 0; i < length; i++)
 	{
 		dst[addr_d + i] = src[addr_s + i];
@@ -62,34 +62,16 @@ void Memory::memcopy(unsigned char* src, unsigned char* dst, unsigned int addr_s
 
 void Memory::clear()
 {
+	std::lock_guard<std::mutex> lock(memMutex);
 	for (int i = 0; i < size; i++) {
 		mem[i] = 0;
 	}
-
-	int a = 10;
 }
 
-void Memory::assertConditions(unsigned int addr)
+void Memory::assertConditions(unsigned int addr) const
 {
 	if (addr > size)
 	{
 		throw 0x01; // memory address out of range
 	}
-
-	while (blocked) 
-	{
-		// wait!
-	}
-
-	blockSemaphore();
-}
-
-void Memory::blockSemaphore()
-{
-	blocked = true;
-}
-
-void Memory::leaseSemaphore()
-{
-	blocked = false;
 }
--- src/vmachine/Memory.h
+++ src/vmachine/Memory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <mutex>
 
 //#include "VMachine.h"
 
@@ -26,11 +27,15 @@ Position:
 
 */
 
-
 class Memory
 {
 public:
+	const int size = 64 * 1024;
+	unsigned char mem[64 * 1024] = { };
+
 	Memory() { }
+
+	static void memcopy(unsigned char* src, unsigned char* dst, unsigned int addr_s, unsigned int addr_d, unsigned int length);
 
 	unsigned char fetchByte(unsigned int addr);
 	
@@ -44,20 +49,11 @@ public:
 
 	void storeDWord(unsigned int addr, double dword);
 
-	static void memcopy(unsigned char* src, unsigned char* dst, unsigned int addr_s, unsigned int addr_d, unsigned int length);
-
 	void clear();
-
-	unsigned int size = 64 * 1024;
-	unsigned char mem[64 * 1024] = { };
 
 	~Memory() { }
 
-private:
-	
-	void assertConditions(unsigned int addr);
-	void blockSemaphore();
-	void leaseSemaphore();
-
-	bool blocked = false;
+private:		
+	std::mutex memMutex;
+	void assertConditions(unsigned int addr) const;	
 };
