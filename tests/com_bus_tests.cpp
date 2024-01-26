f30a4a7d3f2a9571c85756c7b4875c657ac60b7b
jradlak <jradlak@gmail.com>
2024-01-26 22:26:28 +0100

    simple communication bus

--- tests/com_bus_tests.cpp
+++ tests/com_bus_tests.cpp
@@ -4,17 +4,28 @@
 
 #include "../src/infrastructure/CommunicationBus.h"
 
-using namespace com_bus;
+namespace com_bus_tests {
+	using namespace com_bus;
 
-TEST(CommunicationBus, test1)
-{
-	auto bus_data { std::make_unique<Tbus_data>() };
-	
-	publish_command(*bus_data, RocketCommand{ 1, 11.13 });
-	publish_command(*bus_data, RocketCommand{ 2, 12.13 });
-	publish_command(*bus_data, RocketCommand{ 3, 13.13 });
+	TEST(CommunicationBus, simple_single_threaded)
+	{
+		auto bus_data{ std::make_unique<Tbus_data>() };
 
-	RocketCommand command1 { get_command(*bus_data, 123) };
-	RocketCommand command2 { get_command(*bus_data, 223) };
-	RocketCommand command3 { get_command(*bus_data, 323) };
+		publish_command(*bus_data, RocketCommand{ 1, 11.13 });
+		publish_command(*bus_data, RocketCommand{ 2, 12.13 });
+		publish_command(*bus_data, RocketCommand{ 3, 13.13 });
+
+		RocketCommand command1{ get_command(*bus_data, 123) };
+		RocketCommand command2{ get_command(*bus_data, 223) };
+		RocketCommand command3{ get_command(*bus_data, 323) };
+
+		EXPECT_EQ(command1.code(), 1);
+		EXPECT_EQ(command1.value(), 11.13);
+
+		EXPECT_EQ(command2.code(), 2);
+		EXPECT_EQ(command2.value(), 12.13);
+
+		EXPECT_EQ(command3.code(), 3);
+		EXPECT_EQ(command3.value(), 13.13);
+	}
 }
