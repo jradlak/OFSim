45c58bd9ead7e97f6eac95606ed5394f059d722d
jradlak <jradlak@gmail.com>
2024-01-29 11:58:14 +0100

    minor fixes

--- src/infrastructure/RocketCommand.h
+++ src/infrastructure/RocketCommand.h
@@ -4,8 +4,8 @@ class RocketCommand
 {
 public:
 	RocketCommand(int code, double value) { _code = code; _value = value; }
-	inline int code() { return _code; }
-	inline double value() { return _value; }
+	inline int code() const { return _code; }
+	inline double value() const { return _value; }
 
 private:
 	int _code;
