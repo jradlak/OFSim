9ba5ace8aad00874af1facd84fc2dc6bf5033265
jradlak <jradlak@gmail.com>
2024-03-20 19:37:43 +0100

    changelog update. improvemenst in the balistic python program

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Orbital Flight Simulator
 
 ** Change list **
 
-0.22.0 - recognize translation errors 
+0.23.1 - Fixes in the planet texturying. Chaneg size of the planet. point (0, 0, 0) is now in the center of the planet and the origin of the simulation.
+0.23.0 - Python integration. Remove Virtual Machine.
+0.22.2 - Fixes int simulation resetting process. Now full reset is possible when simulation is stopped.
+0.22.1 - Code edito line numbers
--- src/orb_progs/orbital_ballistic.py
+++ src/orb_progs/orbital_ballistic.py
@@ -16,7 +16,15 @@ while float(orbital_data["altitude"]) < 1.5 and not orbital_data["shouldStop"]:
     time.sleep(0.5)
 
 #changing the rotation of the rocket:
-for i in range(0, 12):
+print(' przed rotacja !!!!')
+for i in range(0, 24):
+    print(' wewn¹trz pêtli !!!!')
     orbital_fs.orbital_rocket_rotation_change([1.27, -1.27, 0])
     print("rotating rocket")
     time.sleep(0.2)
+
+while float(orbital_data["altitude"]) < 80 and not orbital_data["shouldStop"]:
+    orbital_data = orbital_fs.get_orbital_data()
+    time.sleep(0.2)
+
+orbital_fs.orbital_thrust_magnitude_change(0.0001)
